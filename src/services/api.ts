04286a847b0284ea773c5faefbf619f2a9875e2d
sakoen <sakoen_not_death@yahoo.com>
2025-12-25 23:33:28 +0700

    forward to Home if error 500. 20251225

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import router from "~/router";
 
 const api = axios.create({
     baseURL: "http://www.myproject.local:8080", // change to your backend URL
@@ -17,6 +18,15 @@ api.interceptors.response.use(
             err.response?.data?.error ||
             err.message ||
             "Unknown error";
+
+        if (err.response?.status === 500) {
+            // prevent redirect loop
+            if (router.currentRoute.value.name !== "home") {
+                setTimeout(() => {
+                    router.replace({name: "home"});
+                }, 1500);
+            }
+        }
 
         return Promise.reject(new Error(message));
     }
